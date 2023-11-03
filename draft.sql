490ddd5d5df731446cb568054d0ceec3e69f4dbf
ham <mddlhl@163.com>
2023-11-02 19:40:40 -0700

    update for lab7

--- draft.sql
+++ draft.sql
@@ -1,245 +1,53 @@
--- SQLite
-DROP TABLE product;
-DROP TABLE pc;
-DROP TABLE laptop;
-DROP TABLE Printer;
+-- # w_warehousekey, increasing number that is unique across the tuples in the table
+-- # w_name: concatenating the supplier name with “____” and with the name of the nation where the warehouse is located
+-- # w_capacity: triple of the maximum total part size across all the nations(total size is sum of the parts (p size) supplied by the supplier to the customers in a nation.),three warehouses owned by a supplier have the same capacity
+-- # s_suppkey, each supplier has three warehouses
+-- # n_nationkey: have the largest number of lineitems supplied by the supplier that are ordered by customers from that nation.
 
-CREATE TABLE product (
-    maker CHAR(32),
-    model INTEGER PRIMARY KEY,
-    type VARCHAR(20) NOT NULL
+CREATE TABLE warehouse (
+    w_warehousekey INTEGER PRIMARY KEY AUTOINCREMENT,
+    w_name TEXT NOT NULL,
+    w_capacity INTEGER NOT NULL,
+    s_suppkey INTEGER NOT NULL,
+    n_nationkey INTEGER NOT NULL
 );
 
-CREATE TABLE pc (
-    model INTEGER PRIMARY KEY,
-    speed FLOAT,
-    ram INTEGER,
-    hd INTEGER,
-    price DECIMAL(7,2) NOT NULL
-);
+SELECT * FROM(
+    SELECT *,row_number() over (partition by s_suppkey order by l_extendedprice ) as rn
+    FROM(
+        SELECT s_suppkey,l_extendedprice
+        FROM  Supplier,Lineitem
+        WHERE s_suppkey = l_suppkey
+        ORDER BY s_suppkey,l_extendedprice DESC
+    ) AS newtab
+) WHERE rn <= 3;
 
-CREATE TABLE laptop (
-    model INTEGER PRIMARY KEY,
-    speed FLOAT,
-    ram INTEGER,
-    hd INTEGER,
-    screen DECIMAL(4,1),
-    price DECIMAL(7,2) NOT NULL
-);
-
-CREATE TABLE printer (
-    model INTEGER PRIMARY KEY,
-    color BOOL,
-    type VARCHAR(30),
-    price decimal(7,2) NOT NULL
-);
-
-SELECT * FROM product;
-
--- SQLite
-
-DELETE FROM product;
-DELETE FROM pc;
-DELETE FROM laptop;
-DELETE FROM Printer;
-
-
-INSERT INTO product VALUES('A', 1001, 'pc');
-INSERT INTO product VALUES('A', 1002, 'pc');
-INSERT INTO product VALUES('A', 1003, 'pc');
-INSERT INTO product VALUES('A', 2004, 'laptop');
-INSERT INTO product VALUES('A', 2005, 'laptop');
-INSERT INTO product VALUES('A', 2006, 'laptop');
-
-INSERT INTO product VALUES('B', 1004, 'pc');
-INSERT INTO product VALUES('B', 1005, 'pc');
-INSERT INTO product VALUES('B', 1006, 'pc');
-INSERT INTO product VALUES('B', 2007, 'laptop');
-
-INSERT INTO product VALUES('C', 1007, 'pc');
-
-INSERT INTO product VALUES('D', 1008, 'pc');
-INSERT INTO product VALUES('D', 1009, 'pc');
-INSERT INTO product VALUES('D', 1010, 'pc');
-INSERT INTO product VALUES('D', 3004, 'printer');
-INSERT INTO product VALUES('D', 3005, 'printer');
-
-INSERT INTO product VALUES('E', 1011, 'pc');
-INSERT INTO product VALUES('E', 1012, 'pc');
-INSERT INTO product VALUES('E', 1013, 'pc');
-INSERT INTO product VALUES('E', 2001, 'laptop');
-INSERT INTO product VALUES('E', 2002, 'laptop');
-INSERT INTO product VALUES('E', 2003, 'laptop');
-INSERT INTO product VALUES('E', 3001, 'printer');
-INSERT INTO product VALUES('E', 3002, 'printer');
-INSERT INTO product VALUES('E', 3003, 'printer');
-
-INSERT INTO product VALUES('F', 2008, 'laptop');
-INSERT INTO product VALUES('F', 2009, 'laptop');
-
-INSERT INTO product VALUES('G', 2010, 'laptop');
-
-INSERT INTO product VALUES('H', 3006, 'printer');
-INSERT INTO product VALUES('H', 3007, 'printer');
-
-
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1001, 2.66, 1024, 250, 2114);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1002, 2.10, 512, 250, 995);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1003, 1.42, 512, 80, 478);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1004, 2.80, 1024, 250, 649);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1005, 3.20, 512, 250, 630);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1006, 3.20, 1024, 320, 1049);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1007, 2.20, 1024, 200, 510);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1008, 2.20, 2048, 250, 770);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1009, 2.00, 1024, 250, 650);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1010, 2.80, 2048, 300, 770);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1011, 1.86, 2048, 160, 959);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1012, 2.80, 1024, 160, 649);
-INSERT INTO pc(model, speed, ram, hd, price) VALUES(1013, 3.06, 512, 80, 529);
-
-
-INSERT INTO laptop VALUES(2001, 2.00, 2048, 240, 20.1, 3673);
-INSERT INTO laptop VALUES(2002, 1.73, 1024, 80, 17.0, 949);
-INSERT INTO laptop VALUES(2003, 1.80, 512, 60, 15.4, 549);
-INSERT INTO laptop VALUES(2004, 2.00, 512, 60, 13.3, 1150);
-INSERT INTO laptop VALUES(2005, 2.16, 1024, 120, 17.0, 2500);
-INSERT INTO laptop VALUES(2006, 2.00, 2048, 80, 15.4, 1700);
-INSERT INTO laptop VALUES(2007, 1.83, 1024, 120, 13.3, 1429);
-INSERT INTO laptop VALUES(2008, 1.60, 1024, 100, 15.4, 900);
-INSERT INTO laptop VALUES(2009, 1.60, 512, 80, 14.1, 680);
-INSERT INTO laptop VALUES(2010, 2.00, 2048, 160, 15.4, 2300);
-
-
-INSERT INTO Printer(model, color, type, price) VALUES(3001, true, 'ink-jet', 99);
-INSERT INTO Printer(model, color, type, price) VALUES(3002, false, 'laser', 239);
-INSERT INTO Printer(model, color, type, price) VALUES(3003, true, 'laser', 899);
-INSERT INTO Printer(model, color, type, price) VALUES(3004, true, 'ink-jet', 120);
-INSERT INTO Printer(model, color, type, price) VALUES(3005, false, 'laser', 120);
-INSERT INTO Printer(model, color, type, price) VALUES(3006, true, 'ink-jet', 100);
-INSERT INTO Printer(model, color, type, price) VALUES(3007, true, 'laser', 200);
-
-
---Exercise 6.5.1 (page 295)
---a)
-INSERT INTO product(model, maker, type) VALUES (1100, 'C', 'pc');
-INSERT INTO pc VALUES (1100, 3.2, 1024, 180, 2499);
-
---c)
-DELETE FROM pc WHERE hd < 100;
-
---e)
-UPDATE product SET maker = 'A' WHERE maker = 'B';
-
---f)
-UPDATE pc SET ram = ram*2, hd = hd+60;
-
-
-with pc1 as(
-select * from pc join product on pc.model = product.model
-),
-laptop1 as(
-select * from laptop join product on laptop.model = product.model
-)
-select pc1.maker,pc1.model,laptop1.model,min(pc1.price+laptop1.price) 
-from pc1,laptop1 
-where pc1.maker = laptop1.maker 
-group by pc1.maker;
-
-with pc_min as (
-    select maker, pc.model as pc_model, min(price) as pc_price from pc join product on pc.model = product.model group by maker
-),
-laptop_min as (
-    select maker, laptop.model as laptop_model, min(price) as laptop_price from laptop join product on laptop.model = product.model group by maker
-)
-select p.maker, pc_model, laptop_model, pc_price + laptop_price as total_price 
-from pc_min p join laptop_min l on p.maker = l.maker;
-
-with makers as (
-    select maker from product where type = 'pc'
-    intersect
-    select maker from product where type = 'laptop'
-    intersect
-    select maker from product where type = 'printer'
-),
-pc_avg as (
-    select maker, avg(price) as pc_avg from pc join product on pc.model = product.model where maker in (select maker from makers) group by maker
-),
-laptop_avg as (
-    select maker, avg(price) as laptop_avg from laptop join product on laptop.model = product.model where maker in (select maker from makers) group by maker
-),
-printer_avg as (
-    select maker, avg(price) as printer_avg from printer join product on printer.model = product.model where maker in (select maker from makers) group by maker
-)
-select p.maker, pc_avg, laptop_avg, printer_avg 
-from pc_avg p 
-join laptop_avg l on p.maker = l.maker 
-join printer_avg pr on p.maker = pr.maker;
-
-   with makers as (
-       select maker from product where type = 'pc'
-       intersect
-       select maker from product where type = 'laptop'
-       intersect
-       select maker from product where type = 'printer'
-   ),
-   pc_avg as (
-       select maker, avg(price) as pc_avg from pc join product on pc.model = product.model where maker in (select maker from makers) group by maker
-   ),
-   laptop_avg as (
-       select maker, avg(price) as laptop_avg from laptop join product on laptop.model = product.model where maker in (select maker from makers) group by maker
-   ),
-   printer_avg as (
-       select maker, avg(price) as printer_avg from printer join product on printer.model = product.model where maker in (select maker from makers) group by maker
-   )
-   select p.maker, pc_avg, laptop_avg, printer_avg 
-   from pc_avg p 
-   join laptop_avg l on p.maker = l.maker 
-   join printer_avg pr on p.maker = pr.maker;
-
-WITH Maker_Avg AS (
-    SELECT 
-        p.maker,
-        AVG(CASE WHEN p.type = 'pc' THEN pc.price ELSE NULL END) AS pc_avg,
-        AVG(CASE WHEN p.type = 'laptop' THEN l.price ELSE NULL END) AS laptop_avg,
-        AVG(CASE WHEN p.type = 'printer' THEN pr.price ELSE NULL END) AS printer_avg
-    FROM product p
-    LEFT JOIN pc ON p.model = pc.model
-    LEFT JOIN laptop l ON p.model = l.model
-    LEFT JOIN printer pr ON p.model = pr.model
-    GROUP BY p.maker
-    HAVING 
-        COUNT(DISTINCT CASE WHEN p.type = 'pc' THEN p.model END) > 0 AND
-        COUNT(DISTINCT CASE WHEN p.type = 'laptop' THEN p.model END) > 0 AND
-        COUNT(DISTINCT CASE WHEN p.type = 'printer' THEN p.model END) > 0
-)
-SELECT maker, pc_avg, laptop_avg, printer_avg
-FROM Maker_Avg;
-
-with pc_maker as(
-select pc.model,maker,price from product join pc on pc.model = product.model
-),
-laptop_maker as(
-select laptop.model,maker,price from product join laptop on laptop.model = product.model
-),
-printer_maker as(
-select printer.model,maker,price from product join printer on printer.model = product.model
+WITH temp_tb AS(
+    SELECT  *
+    FROM(
+        SELECT  supplier.s_suppkey AS supplier_key,
+                supplier.s_name AS supplier_name,
+                nation.n_nationkey AS cus_nationkey,
+                nation.n_name AS cus_nationname,
+                sum(part.p_size) AS total_size,
+                count(*) AS total_lineitem,
+                row_number() over (partition by supplier.s_suppkey order by count(*) DESC) AS rn
+        FROM    lineitem
+        JOIN    part ON lineitem.l_partkey = part.p_partkey
+        JOIN    supplier ON lineitem.l_suppkey = supplier.s_suppkey
+        JOIN    orders ON lineitem.l_orderkey = orders.o_orderkey
+        JOIN    customer ON orders.o_custkey = customer.c_custkey
+        JOIN    nation ON customer.c_nationkey = nation.n_nationkey
+        GROUP BY supplier.s_suppkey, nation.n_nationkey
+    ) WHERE rn <= 3
 )
 
-select pc_maker.maker,pc_maker.model as pc_model,printer_maker.model as printer_model, laptop_maker.model as laptop_model, pc_maker.price+laptop_maker.price+printer_maker.price as total_price from pc_maker,laptop_maker,printer_maker where pc_maker.maker = printer_maker.maker and pc_maker.maker = laptop_maker.maker order by total_price asc limit 1
+INSERT INTO warehouse (w_warehousekey,w_name,w_capacity,w_suppkey,w_nationkey)
+SELECT  row_number() OVER () AS w_warehousekey,
+        supplier_name || '____' || cus_nationname AS w_name,
+        total_size*3 AS w_capacity,
+        supplier_key AS w_suppkey,
+        cus_nationkey AS w_nationkey
+FROM    temp_tb;
 
-select maker,pc.model from pc,product where pc.model = product.model group by maker having min(price)
-
-select count(distinct pc_model) from (
-)
-
-select count(distinct pc.model) from pc
-
-select  
-    maker,count(distinct printer.model),count(distinct pc.model) 
-from 
-    product
-left outer join 
-    pc on pc.model = product.model
-left outer join
-    printer on printer.model = product.model
-group by maker
+SELECT * FROM warehouse;
--- lab-7/Lab_7.py
+++ lab-7/Lab_7.py
@@ -1,6 +1,30 @@
 import sqlite3
 from sqlite3 import Error
 
+# createTable creates a table warehouse and adds it to the TPC-H database. Table warehouse stores
+# data on the warehouses suppliers own and has the following schema/attributes:
+# w_warehousekey decimal(9,0) not null,
+# w_name char(100) not null,
+# w_capacity decimal(6,0) not null,
+# w_suppkey decimal(9,0) not null,
+# w_nationkey decimal(2,0) not null
+# w warehousekey is the unique identifier of the warehouse and has a numeric value. w name is the
+# name of the warehouse, while w capacity is its capacity. w suppkey is the identifier of the supplier
+# that owns the warehouse. A warehouse is owned by a single supplier, while a supplier can own
+# multiple warehouses. w nationkey is the identifier of the nation where the warehouse is located.
+# w suppkey and w nationkey take values from the corresponding attributes in tables supplier and
+# nation, respectively.
+# •dropTable drops/eliminates table warehouse together with all its data from the TPC-H database.
+# •populateTable populates table warehouse with tuples corresponding to every supplier in the database.
+# Three warehouses are created for every supplier. The nations where these warehouses are located are
+# those that have the largest number of lineitems supplied by the supplier that are ordered by customers
+# from that nation. In case of equality, the nations are sorted in alphabetical order and the first three
+# are selected. The name of a warehouse is obtained by concatenating the supplier name with “ ” and
+# with the name of the nation where the warehouse is located. In order to determine the capacity of a
+# warehouse, you have to compute the total size of the parts (p size) supplied by the supplier to the
+# customers in a nation. Then, the warehouse capacity is taken as the triple of the maximum total part
+# size across all the nations. The three warehouses owned by a supplier have the same capacity. Finally,
+# the w warehousekey value is set as an increasing number that is unique across the tuples in the table.
 
 def openConnection(_dbFile):
     print("++++++++++++++++++++++++++++++++++")
@@ -36,15 +60,13 @@ def createTable(_conn):
 
     try:
         _conn.execute("""
-            CREATE TABLE Warehouse (
-                W_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                W_NAME TEXT NOT NULL,
-                W_CAPACITY INTEGER NOT NULL,
-                S_ID INTEGER,
-                N_ID INTEGER,
-                FOREIGN KEY (S_ID) REFERENCES Supplier(S_ID),
-                FOREIGN KEY (N_ID) REFERENCES Nation(N_ID)
-            );
+        CREATE TABLE IF NOT EXISTS warehouse (
+            w_warehousekey decimal(9,0) not null,
+            w_name char(100) not null,
+            w_capacity decimal(6,0) not null,
+            w_suppkey decimal(9,0) not null,
+            w_nationkey decimal(2,0) not null
+        );
         """)
         print("Table created successfully")
     except Error as e:
@@ -56,17 +78,52 @@ def createTable(_conn):
 def dropTable(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Drop tables")
-
+    try:
+        _conn.execute("DROP TABLE IF EXISTS warehouse;")
+        print("success")
+    except Error as e:
+        print(e)
     print("++++++++++++++++++++++++++++++++++")
-
 
 def populateTable(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Populate table")
+    try:
+        _conn.execute("""
+        WITH temp_tb AS(
+        SELECT  *
+        FROM(
+            SELECT  supplier.s_suppkey AS supplier_key,
+                    supplier.s_name AS supplier_name,
+                    nation.n_nationkey AS cus_nationkey,
+                    nation.n_name AS cus_nationname,
+                    sum(part.p_size) AS total_size,
+                    count(*) AS total_lineitem,
+                    row_number() over (partition by supplier.s_suppkey order by count(*) DESC) AS rn
+            FROM    lineitem
+            JOIN    part ON lineitem.l_partkey = part.p_partkey
+            JOIN    supplier ON lineitem.l_suppkey = supplier.s_suppkey
+            JOIN    orders ON lineitem.l_orderkey = orders.o_orderkey
+            JOIN    customer ON orders.o_custkey = customer.c_custkey
+            JOIN    nation ON customer.c_nationkey = nation.n_nationkey
+            GROUP BY supplier.s_suppkey, nation.n_nationkey
+            ) WHERE rn <= 3
+        )
 
+        INSERT INTO warehouse (w_warehousekey,w_name,w_capacity,w_suppkey,w_nationkey)
+        SELECT  row_number() OVER () AS w_warehousekey,
+                supplier_name || '____' || cus_nationname AS w_name,
+                total_size*3 AS w_capacity,
+                supplier_key AS w_suppkey,
+                cus_nationkey AS w_nationkey
+        FROM    temp_tb;
+        """)
+        print("success")
+    except Error as e:
+        print(e)
     print("++++++++++++++++++++++++++++++++++")
 
-
+# q1: displays the entire content of the warehouse table sorted on w warehousekey by performing a SQL query
 def Q1(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Q1")
@@ -77,13 +134,20 @@ def Q1(_conn):
         header = "{:>10} {:<40} {:>10} {:>10} {:>10}"
         output.write((header.format("wId", "wName", "wCap", "sId", "nId")) + '\n')
 
+        cursor = _conn.cursor()
+        cursor.execute("""
+        SELECT  *  FROM warehouse ORDER BY w_warehousekey;
+        """)
+        rows = cursor.fetchall()
+        for row in rows:
+            output.write(("{:>10} {:<40} {:>10} {:>10} {:>10}".format(row[0], row[1], row[2], row[3], row[4])) + '\n')
         output.close()
     except Error as e:
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
 
-
+# q2: computes the number of warehouses and the total capacity for the warehouses in every nation, result is sorted in decreasing order of the number of warehouses and of the capacity, then alphabetical order of the nation name
 def Q2(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Q2")
@@ -94,13 +158,30 @@ def Q2(_conn):
         header = "{:<40} {:>10} {:>10}"
         output.write((header.format("nation", "numW", "totCap")) + '\n')
 
+        cursor = _conn.cursor()
+        cursor.execute("""
+        SELECT  nation.n_name AS nation,
+                count(*) AS numW,
+                sum(w_capacity) AS totCap
+        FROM    warehouse
+        JOIN    nation ON warehouse.w_nationkey = nation.n_nationkey
+        GROUP BY nation.n_name
+        ORDER BY numW DESC, totCap DESC, nation.n_name;
+        """)
+        rows = cursor.fetchall()
+        for row in rows:
+            output.write(("{:<40} {:>10} {:>10}".format(row[0], row[1], row[2])) + '\n')
+        
         output.close()
     except Error as e:
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
 
-
+# q3: computes the suppliers that have a warehouse in a given nation taken as input parameter. The
+# nation where warehouses are located is read from the input file input/3.in. Q3 prints the name of the
+# supplier, the nation of the supplier, and the name of the warehouse—sorted in alphabetical order by
+# supplier name.
 def Q3(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Q3")
@@ -115,14 +196,30 @@ def Q3(_conn):
 
         header = "{:<20} {:<20} {:<40}"
         output.write((header.format("supplier", "nation", "warehouse")) + '\n')
-
+        
+        cursor = _conn.cursor()
+        cursor.execute("""
+        SELECT  supplier.s_name AS supplier,
+                nation.n_name AS nation,
+                warehouse.w_name AS warehouse
+        FROM    warehouse
+        JOIN    supplier ON warehouse.w_suppkey = supplier.s_suppkey
+        JOIN    nation ON warehouse.w_nationkey = nation.n_nationkey
+        WHERE   nation.n_name = ?
+        ORDER BY supplier.s_name;
+        """, (nation,))
+        rows = cursor.fetchall()
+        for row in rows:
+            output.write(("{:<20} {:<20} {:<40}".format(row[0], row[1], row[2])) + '\n')
+        
         output.close()
     except Error as e:
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
 
-
+# q4 finds the warehouses from a given region that have capacity larger than a given threshold. The region name and the minimum capacity are parameters stored in the file input/4.in. Q4 prints the
+# warehouse name and its capacity in decreasing order of the capacity.
 def Q4(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Q4")
@@ -133,19 +230,34 @@ def Q4(_conn):
         cap = input.readline().strip()
         input.close()
 
-
         output = open('output/4.out', 'w')
 
         header = "{:<40} {:>10}"
         output.write((header.format("warehouse", "capacity")) + '\n')
-
+        
+        cursor = _conn.cursor()
+        cursor.execute("""
+        SELECT  w_name AS warehouse,
+                w_capacity AS capacity
+        FROM    warehouse
+        JOIN    nation ON warehouse.w_nationkey = nation.n_nationkey
+        JOIN    region ON nation.n_regionkey = region.r_regionkey
+        WHERE   region.r_name = ? AND w_capacity > ?
+        ORDER BY w_capacity DESC;
+        """, (region, cap))
+        rows = cursor.fetchall()
+        for row in rows:
+            output.write(("{:<40} {:>10}".format(row[0], row[1])) + '\n')
+    
         output.close()
     except Error as e:
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
 
-
+# q5: determines the total capacity of the warehouses belonging to suppliers from a given nation in every region. The suppliers' nation is a parameter stored in the file input/5.in. If there are no warehouses
+# in a region, then value 0 is printed for that region. Q5 prints the region and the capacity sorted
+# alphabetically by region.
 def Q5(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Q5")
@@ -161,6 +273,20 @@ def Q5(_conn):
         header = "{:<20} {:>20}"
         output.write((header.format("region", "capacity")) + '\n')
 
+        cursor = _conn.cursor()
+        cursor.execute("""
+        SELECT  region.r_name AS region,
+                sum(w_capacity) AS capacity
+        FROM    warehouse
+        JOIN    nation ON warehouse.w_nationkey = nation.n_nationkey
+        JOIN    region ON nation.n_regionkey = region.r_regionkey
+        WHERE   nation.n_name = ?
+        GROUP BY region.r_name
+        ORDER BY region.r_name;
+        """, (nation,))
+        rows = cursor.fetchall()
+        for row in rows:
+            output.write(("{:<20} {:>20}".format(row[0], row[1])) + '\n')
         output.close()
     except Error as e:
         print(e)
